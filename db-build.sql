c6bc990a8ef03fac2c2f9803ed5c86b7e24eba83
Daniel Fucci <daniel.m.fucci@gmail.com>
2021-05-06 20:18:49 -0300

    Correcting request security

--- db-build.sql
+++ db-build.sql
@@ -1,14 +1,14 @@
 -- USERS --
 CREATE TABLE users
 (
-    id           INT(11) NOT NULL AUTO_INCREMENT,
-    email        VARCHAR(255) NOT NULL,
-    name         VARCHAR(255) NOT NULL,
-    password     VARCHAR(255) NOT NULL,
-    enabled      BOOL NOT NULL,
-    created_at   TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at   TIMESTAMP NULL,
-    deleted_at   TIMESTAMP NULL,
+    id         INT(11) NOT NULL AUTO_INCREMENT,
+    email      VARCHAR(255) NOT NULL,
+    name       VARCHAR(255) NOT NULL,
+    password   VARCHAR(255) NOT NULL,
+    enabled    BOOL         NOT NULL,
+    created_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NULL,
+    deleted_at TIMESTAMP NULL,
     PRIMARY KEY (id)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8
@@ -17,12 +17,12 @@ CREATE TABLE users
 -- AUTHORITIES --
 CREATE TABLE authorities
 (
-    id           INT(11) NOT NULL AUTO_INCREMENT,
-    username     VARCHAR(255) NOT NULL,
-    authority    VARCHAR(255) NOT NULL,
-    created_at   TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at   TIMESTAMP NULL,
-    deleted_at   TIMESTAMP NULL,
+    id         INT(11) NOT NULL AUTO_INCREMENT,
+    username   VARCHAR(255) NOT NULL,
+    authority  VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NULL,
+    deleted_at TIMESTAMP NULL,
     PRIMARY KEY (id)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8
--- src/main/java/com/danielfucci/watchman/security/SecurityConfig.java
+++ src/main/java/com/danielfucci/watchman/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.danielfucci.watchman.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -29,13 +30,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .dataSource(dataSource)
                 .usersByUsernameQuery(
                         "select email, password, enabled from users " +
-                        "where email=?"
+                                "where email=?"
                 )
                 .passwordEncoder(encoder());
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("/user/register").permitAll();
+        http
+                .cors().and().csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/user/register").permitAll()
+                .anyRequest().authenticated();
+        ;
     }
 }
